cdf64d192af62d0eda1ae9e31d58c587a607ff87
kaifu.zdl <kaifu.zdl@alibaba-inc.com>
2016-02-01 10:34:03 +0800

    提交代码

--- src/com/suken/bridgedetection/http/HttpTask.java
+++ src/com/suken/bridgedetection/http/HttpTask.java
@@ -25,9 +25,9 @@ import android.text.TextUtils;
 
 public class HttpTask {
 
-	public static String REQUEST_IP = "binglee.wicp.net";
+	public static String REQUEST_IP = "121.28.74.58";
 
-	public static String REQUEST_PORT = "";
+	public static String REQUEST_PORT = "8080";
 
 	private RequestType mRequestType = null;
 
